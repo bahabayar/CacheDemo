6c3bd7139a73940fe6615a0e88a5f136f4347afa
bahabayar <69486803+bahabayar@users.noreply.github.com>
2021-02-21 02:00:40 +0300

    Cache

---
+++ CacheExamplee/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CacheExamplee.EntityContext;
+using System.Runtime.Caching;
+
+
+namespace CacheExamplee.Controllers
+{
+    public class ProductController : Controller
+    {
+      
+
+        // GET: Product
+        
+        public ActionResult List()
+        {
+
+            var cache = MemoryCache.Default;
+            var policy =  new CacheItemPolicy().AbsoluteExpiration=DateTime.Now.AddMinutes(1);
+
+
+            List<Product> productList = new List<Product>();
+            productList = (List<Product>) cache.Get("productList");
+            if (productList == null)
+            {
+                using (var context = new NorthwindEntities())
+                {
+                    productList = context.Products.ToList();
+                    cache.Set("productList", productList,policy);
+                }
+            }
+            else
+            {
+
+            }
+
+            return View(productList);
+        }
+
+        
+    }
+}
